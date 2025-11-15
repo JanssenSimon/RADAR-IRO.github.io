dd11f09aac0aed8a7707423c196a84b190aba6f9
simon <simon.janssen@umontreal.ca>
2025-11-14 21:57:19 -0500

    Documents homepage blog highlighting utility and has it display blog entry titles. Also dates are gotten from RSS feed metadata instead of OS file stats

--- utils.jl
+++ utils.jl
@@ -16,16 +16,21 @@ function lx_baz(com, _)
 end
 
 function hfun_homepage_list()::String
+    # Get markdown files from /blog/
     list = readdir("blog")
     filter!(f -> endswith(f, ".md"), list)
-    dates = [stat(joinpath("blog", f)).mtime for f in list]
+
+    # Sort files by recency and get 10 first ones
+    dates = [pagevar(joinpath("blog", f), "rss_pubdate") for f in list]
     perm = sortperm(dates, rev=true)
     idxs = perm[1:min(10, end)]
+
+    # Create list of links
     io = IOBuffer()
     write(io, "<ul>")
-    for (k, i) in enumerate(idxs)
-        fi = "/blog/" * splitext(list[i])[1] * "/"
-        write(io, """<li><a href="$fi">Post $k</a></li>\n""")
+    for (k,i) in enumerate(idxs)
+        fi = "/blog/" * first(splitext(list[i])) * "/"
+        write(io, """<li><a href="$fi">$(pagevar(joinpath("blog", list[i]), "title"))</a></li>\n""")
     end
     write(io, "</ul>")
     return String(take!(io))
